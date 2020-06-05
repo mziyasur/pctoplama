083399234eceb030f0f362a517633a7dbea18a06
Esli <Esli@Esli-Bilgisayar>
2020-06-05 17:52:46 +0300

    EŞREF YAĞAR Login controller classında ve login xhtml sayfalarında bazı düzenlemeler yapılarak ad veya parolaya bağlı giriş hatası alamama durumu düzeltildi. Login xhtml sayfalarındaki validatorler düzenlendi. Görünümle ilgili bazı değişiklikler yapıldı.

--- src/java/util/LoginController.java
+++ src/java/util/LoginController.java
@@ -9,6 +9,7 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.faces.component.UIComponent;
 
 @Named
 @SessionScoped
@@ -22,7 +23,11 @@ public class LoginController implements Serializable {
     private AdminDAO adminDao;
     public static Admin tmp;
 
-    public String kullaniciLogin() {
+    private UIComponent kulbutton;
+    private UIComponent adbutton;
+    
+    public String kullaniciLogin()  {
+
         
         temp = this.getKullaniciDao().findByusername(this.kullanici.getKullanici_ad());
         String parola = this.kullanici.getKullanici_sifre();
@@ -30,13 +35,23 @@ public class LoginController implements Serializable {
             if (parola.equals(temp.getKullanici_sifre())) {
                 this.kullanici = temp;
                 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("valid_kullanici", this.kullanici);
+                kullanici.setKullanici_ad("");
+                kullanici.setKullanici_sifre("");
                 return "/oem/list.xhtml";
             } else {
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(parola + " - " + temp.getKullanici_sifre()));
+                FacesMessage message = new FacesMessage("Hatalı şifre!");
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(kulbutton.getClientId(context), message);
+                kullanici.setKullanici_sifre("");
                 return "/login/kullanicilogin.xhtml";
+
             }
         } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Hatalı Kullanıcı veya Şifre!"));
+            FacesMessage message = new FacesMessage("Bu isimde bir kullanıcı mevcut değil!");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(kulbutton.getClientId(context), message);
+            kullanici.setKullanici_ad("");
+            kullanici.setKullanici_sifre("");
             return "/login/kullanicilogin.xhtml";
         }
     }
@@ -49,16 +64,25 @@ public class LoginController implements Serializable {
             if (p.equals(tmp.getAdmin_sifre())) {
                 this.admin = tmp;
                 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("valid_admin", this.admin);
+                admin.setAdmin_ad("");
+                admin.setAdmin_sifre("");
                 return "/parca/anakart/list.xhtml";
             } else {
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(p + " - " + tmp.getAdmin_sifre()));
+                FacesMessage message = new FacesMessage("Hatalı şifre!");
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(adbutton.getClientId(context), message);
+                admin.setAdmin_sifre("");
                 return "/login/adminlogin.xhtml";
             }
         } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Hatalı Kullanıcı veya Şifre!"));
+            FacesMessage message = new FacesMessage("Bu isimde bir admin mevcut değil!");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(adbutton.getClientId(context), message);
+            admin.setAdmin_ad("");
+            admin.setAdmin_sifre("");
             return "/login/adminlogin.xhtml";
         }
-
+        
     }
 
     public Kullanici getKullanici() {
@@ -103,6 +127,22 @@ public class LoginController implements Serializable {
 
     public void setAdminDao(AdminDAO adminDao) {
         this.adminDao = adminDao;
+    }
+
+    public UIComponent getKulbutton() {
+        return kulbutton;
+    }
+
+    public void setKulbutton(UIComponent kulbutton) {
+        this.kulbutton = kulbutton;
+    }
+
+    public UIComponent getAdbutton() {
+        return adbutton;
+    }
+
+    public void setAdbutton(UIComponent adbutton) {
+        this.adbutton = adbutton;
     }
 
 }
